75b62a9065c304f9f2c7c3a37ba14b58852fd30a
majmalmdt <majmalmdt@gmail.com>
2026-01-28 20:39:16 +0400

    Add Dockerfile, docker-compose.yml, and update requirements for project setup

--- app/vector_store/chroma_client.py
+++ app/vector_store/chroma_client.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 import chromadb
 from app.core.config import CHROMA_DB_DIR
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 PERSIST_DIR = BASE_DIR / CHROMA_DB_DIR
 
 PERSIST_DIR.mkdir(parents=True, exist_ok=True)
 
-chroma_client = chromadb.PersistentClient(path=str(PERSIST_DIR))
+chroma_client = chromadb.PersistentClient(
+    path=str(PERSIST_DIR)
+)
 
-collection = chroma_client.get_or_create_collection(name="knowledge_collection")
-
+collection = chroma_client.get_or_create_collection(
+    name="knowledge_collection"
+)
